2a704ed4b3d0433164c3d3c3005fc0b114e7b407
prabhudeep <prabhudeep.m@e-commsys.com.au>
2025-05-13 12:45:37 +0530

    Product Database Table

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-USE Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
