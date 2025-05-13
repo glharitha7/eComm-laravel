5bba9ca6d944770fccfabb053474c92688ae76a2
prabhudeep <prabhudeep.m@e-commsys.com.au>
2025-05-13 12:04:42 +0530

    Set Up Database and Login Module

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,17 +3,18 @@
 <div class="container d-flex justify-content-center align-items-center" style="height: 60vh;">
   <div class="col-md-4">
     <h2 class="text-center">Login</h2>
-    <form>
+    <form action='/login' method="POST">
       <!-- Email Input -->
       <div class="mb-3">
+        @csrf
         <label for="email" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter your email" required>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email" required>
       </div>
 
       <!-- Password Input -->
       <div class="mb-3">
         <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" placeholder="Enter your password" required>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password" required>
       </div>
 
       <!-- Remember Me Checkbox -->
